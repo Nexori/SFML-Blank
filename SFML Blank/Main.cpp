20ca50876a5a2c76d7ed82057483edd2c90f55f2
Krzysztof Sauer <sauer1999@hotmail.com>
2019-10-05 23:12:02 +0200

    Updated main data

--- SFML Blank/Main.cpp
+++ SFML Blank/Main.cpp
@@ -1,6 +1,30 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
-
+using namespace sf;
 int main() {
-	system("pause");
+
+		//srand(time(NULL));
+		const int WINX = 1400;
+		const int WINY = 800;
+		RenderWindow window(VideoMode(WINX, WINY), "Wingman Game", Style::Default);
+
+		Clock clock;
+		float dt = 0.f;
+
+		//Game loop
+		while (window.isOpen())
+		{
+			Event event;
+			while (window.pollEvent(event))
+			{
+				if (event.type == Event::Closed)
+					window.close();
+				if (event.type == Event::KeyPressed && event.key.code == Keyboard::Escape)
+					window.close();
+			}
+
+			dt = clock.restart().asSeconds();
+		}
+
+		return 0;
 }
